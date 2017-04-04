5739b7e2186c013d559b9cdea8b7e44d59bcfb5a
Ming-Yi Lai <s104062201@m104.nthu.edu.tw>
2017-04-04 23:45:33 +0800

    ADD: exception unit test

--- src/pro/gradeSystem/UITest.java
+++ src/pro/gradeSystem/UITest.java
@@ -70,5 +70,81 @@ public class UITest {
 		testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
+	
+	@Test
+	public void testNoSuchCommandExceptions_1() {
+		String testCase = "955002056\nK\n";
+		System.setIn(new ByteArrayInputStream(testCase.getBytes()));
+		
+		String assertResult = "輸入 ID 或 Q(結束使用)？"
+				+ "Welcome 許文馨\n"
+				+ "輸入指令\t1) G 顯示成績 (Grade)\n"
+				+ "\t2) R 顯示排名 (Rank)\n"
+				+ "\t3) A 顯示平均 (Average)\n"
+				+ "\t4) W 更新配分 (Weight)\n"
+				+ "\t5) E 離開選單 (Exit)\n"
+				+ "使用者輸入："
+				+ "指令錯了！\n"
+				+ "結束了\n";
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		testUI = new UI();
+		assertEquals(assertResult, outContent.toString());
+	}
+	
+	@Test
+	public void testNoSuchCommandExceptions_2() {
+		String testCase = "955002056\nR\nK\n";
+		System.setIn(new ByteArrayInputStream(testCase.getBytes()));
+		
+		String assertResult = "輸入 ID 或 Q(結束使用)？"
+				+ "Welcome 許文馨\n"
+				+ "輸入指令\t1) G 顯示成績 (Grade)\n"
+				+ "\t2) R 顯示排名 (Rank)\n"
+				+ "\t3) A 顯示平均 (Average)\n"
+				+ "\t4) W 更新配分 (Weight)\n"
+				+ "\t5) E 離開選單 (Exit)\n"
+				+ "使用者輸入："
+				+ "許文馨排名第9\n"
+				+ "使用者輸入："
+				+ "指令錯了！\n"
+				+ "結束了\n";
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		testUI = new UI();
+		assertEquals(assertResult, outContent.toString());
+	}
+	
+	@Test
+	public void testNoSuchIDExceptions_1() {
+		String testCase = "955002050\n";
+		System.setIn(new ByteArrayInputStream(testCase.getBytes()));
+		
+		String assertResult = "輸入 ID 或 Q(結束使用)？"
+				+ "ID錯了！\n"
+				+ "結束了\n";
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		testUI = new UI();
+		assertEquals(assertResult, outContent.toString());
+	}
+	
+	@Test
+	public void testNoSuchIDExceptions_2() {
+		String testCase = "-1234\n";
+		System.setIn(new ByteArrayInputStream(testCase.getBytes()));
+		
+		String assertResult = "輸入 ID 或 Q(結束使用)？"
+				+ "ID錯了！\n"
+				+ "結束了\n";
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		testUI = new UI();
+		assertEquals(assertResult, outContent.toString());
+	}
 
 }
