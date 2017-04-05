2f155a0df0841d13449d692c0f84d16b21317d0e
Ming-Yi Lai <s104062201@m104.nthu.edu.tw>
2017-04-05 12:40:17 +0800

    ADD: headers for all methods in `UITest.java`

--- src/pro/gradeSystem/UITest.java
+++ src/pro/gradeSystem/UITest.java
@@ -11,22 +11,58 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/*
+ * class UITest
+ * ---
+ * class UI的JUnit Test
+ * 
+ * Member Data: 
+ * 1. stdin // 修改前的System.in
+ * 2. stdout // 修改前的System.out
+ * 
+ * Member Function:
+ * 1. tearDown() // 結束測試後的清理
+ * 2. testUI_1() // method UI()測試1
+ * 3. testUI_2() // method UI()測試2
+ * 4. testNoSuchCommandExceptions_1() // NoSuchCommandExceptions.NoSuchCommandExceptions()測試1
+ * 5. testNoSuchCommandExceptions_2() // NoSuchCommandExceptions.NoSuchCommandExceptions()測試2
+ * 6. testNoSuchIDExceptions_1() // NoSuchIDExceptions.NoSuchIDExceptions()測試1
+ * 7. testNoSuchIDExceptions_2() // NoSuchIDExceptions.NoSuchIDExceptions()測試2
+ */
 public class UITest {
-	private UI testUI;
 	private final InputStream stdin = System.in;
 	private final PrintStream stdout = System.out;
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
+	/*
+	 * method tearDown()
+	 * ---
+	 * 結束測試後的清理
+	 * 
+	 * Pseudo Code: 
+	 * 1. 回復System.in及System.out的原始設定
+	 */
 	@After
 	public void tearDown() throws Exception {
-		testUI = null;
 		System.setIn(stdin);
 		System.setOut(stdout);
 	}
 
+	/*
+	 * method testUI_1()
+	 * ---
+	 * method UI()測試1
+	 * 
+	 * Pseudo Code: 
+	 * 1. 將System.in設爲testCase的InputStream
+	 * 2. 將終端輸出導至outContent
+	 * 3. 執行UI()
+	 * 4. 檢查終端輸出是否與預期一致
+	 * 
+	 * Terminal Input Test Case: 
+	 * 955002056 // 輸入學號
+	 * E // 5) E 離開選單 (Exit)
+	 * Q // 離開系統
+	 */
 	@Test
 	public void testUI_1() {
 		String testCase = "955002056\nE\nQ\n";
@@ -44,10 +80,27 @@ public class UITest {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 		
-		testUI = new UI();
+		UI testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
 	
+	/*
+	 * method testUI_2()
+	 * ---
+	 * method UI()測試2
+	 * 
+	 * Pseudo Code: 
+	 * 1. 將System.in設爲testCase的InputStream
+	 * 2. 將終端輸出導至outContent
+	 * 3. 執行UI()
+	 * 4. 檢查終端輸出是否與預期一致
+	 * 
+	 * Terminal Input Test Case: 
+	 * 955002056 // 輸入學號
+	 * R // 2) R 顯示排名 (Rank)
+	 * E // 5) E 離開選單 (Exit)
+	 * Q // 離開系統
+	 */
 	@Test
 	public void testUI_2() {
 		String testCase = "955002056\nR\nE\nQ\n";
@@ -67,10 +120,25 @@ public class UITest {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 		
-		testUI = new UI();
+		UI testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
 	
+	/*
+	 * method testNoSuchCommandExceptions_1()
+	 * ---
+	 * NoSuchCommandExceptions.NoSuchCommandExceptions()測試1
+	 * 
+	 * Pseudo Code: 
+	 * 1. 將System.in設爲testCase的InputStream
+	 * 2. 將終端輸出導至outContent
+	 * 3. 執行UI()
+	 * 4. 檢查終端輸出是否與預期一致
+	 * 
+	 * Terminal Input Test Case: 
+	 * 955002056 // 輸入學號
+	 * K // 不存在的指令
+	 */
 	@Test
 	public void testNoSuchCommandExceptions_1() {
 		String testCase = "955002056\nK\n";
@@ -89,10 +157,26 @@ public class UITest {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 		
-		testUI = new UI();
+		UI testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
 	
+	/*
+	 * method testNoSuchCommandExceptions_2()
+	 * ---
+	 * NoSuchCommandExceptions.NoSuchCommandExceptions()測試2
+	 * 
+	 * Pseudo Code: 
+	 * 1. 將System.in設爲testCase的InputStream
+	 * 2. 將終端輸出導至outContent
+	 * 3. 執行UI()
+	 * 4. 檢查終端輸出是否與預期一致
+	 * 
+	 * Terminal Input Test Case: 
+	 * 955002056 // 輸入學號
+	 * R // 2) R 顯示排名 (Rank)
+	 * K // 不存在的指令
+	 */
 	@Test
 	public void testNoSuchCommandExceptions_2() {
 		String testCase = "955002056\nR\nK\n";
@@ -113,10 +197,24 @@ public class UITest {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 		
-		testUI = new UI();
+		UI testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
 	
+	/*
+	 * method testNoSuchIDExceptions_1()
+	 * ---
+	 * NoSuchIDExceptions.NoSuchIDExceptions()測試1
+	 * 
+	 * Pseudo Code: 
+	 * 1. 將System.in設爲testCase的InputStream
+	 * 2. 將終端輸出導至outContent
+	 * 3. 執行UI()
+	 * 4. 檢查終端輸出是否與預期一致
+	 * 
+	 * Terminal Input Test Case: 
+	 * 955002050 // 輸入不存在的學號
+	 */
 	@Test
 	public void testNoSuchIDExceptions_1() {
 		String testCase = "955002050\n";
@@ -128,10 +226,24 @@ public class UITest {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 		
-		testUI = new UI();
+		UI testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
 	
+	/*
+	 * method testNoSuchIDExceptions_2()
+	 * ---
+	 * NoSuchIDExceptions.NoSuchIDExceptions()測試2
+	 * 
+	 * Pseudo Code: 
+	 * 1. 將System.in設爲testCase的InputStream
+	 * 2. 將終端輸出導至outContent
+	 * 3. 執行UI()
+	 * 4. 檢查終端輸出是否與預期一致
+	 * 
+	 * Terminal Input Test Case: 
+	 * -1234 // 輸入非預期的格式
+	 */
 	@Test
 	public void testNoSuchIDExceptions_2() {
 		String testCase = "-1234\n";
@@ -143,7 +255,7 @@ public class UITest {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 		
-		testUI = new UI();
+		UI testUI = new UI();
 		assertEquals(assertResult, outContent.toString());
 	}
 
